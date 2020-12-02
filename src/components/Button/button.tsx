9af88f45e57a965af15e08ae2f9ef9c287e8c8b5
chenbingjie <chenbingjie@ghostcloud.cn>
2020-12-02 18:57:14 +0800

    button添加说明文档

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FC, ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react';
 import classnames from 'classnames';
 
 export type ButtonSize = 'lg' | 'sm';
@@ -6,22 +6,29 @@ export type ButtonType = 'primary' | 'default' | 'link' | 'danger';
 
 interface BaseButtonProps {
   className?: string;
+  /** 设置 Button 的禁用 */
   disabled?: boolean;
+  /** 设置 Button 的尺寸 */
   size?: ButtonSize;
+  /** 设置 Button 的类型 */
   btnType?: ButtonType;
   children: React.ReactNode;
   href?: string;
 }
 
 // 原生button上的属性
-type NativeButtonProps = BaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>;
+type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>;
 // 原生a标签上的属性
-type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElement>;
+type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>;
 
 // Partial 把里面所有的属性都变为可选的
 export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
 
-const Button: React.FC<ButtonProps> = (props) => {
+/**
+ * Button 组件
+ */
+
+export const Button: FC<ButtonProps> = (props) => {
   const { btnType, children, size, disabled, href, className, ...restProps } = props;
   const classes = classnames('btn', className, {
     [`btn-${btnType}`]: btnType,
